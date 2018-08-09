0cceafba28146ab0643691a15f74249f50348f8b
Dan Addison <danaddisoncreative@gmail.com>
2018-08-09 16:51:04 +0100

    Override post meta function to omit author card

--- wp-content/themes/storefrontiersman/functions.php
+++ wp-content/themes/storefrontiersman/functions.php
@@ -38,11 +38,65 @@ unset ($orderby['popularity']);
 return $orderby;
 }
     
-// remove search from header:
-add_action( 'init', 'da_remove_storefront_header_search' );
+// remove stuff:
+add_action( 'init', 'da_remove_storefront_actions' );
 
-function da_remove_storefront_header_search() {
+function da_remove_storefront_actions() {
+// remove search from header:
 remove_action( 'storefront_header', 'storefront_product_search', 	40 );
+}
+
+// remove author card from posts (copied the Storefront function and took author card div out, my version of the function now takes precedence)
+if ( ! function_exists( 'storefront_post_meta' ) ) {
+	/**
+	 * Display the post meta
+	 *
+	 * @since 1.0.0
+	 */
+	function storefront_post_meta() {
+		?>
+		<aside class="entry-meta">
+			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search.
+
+			?>
+
+			<?php
+			/* translators: used between list items, there is a space after the comma */
+			$categories_list = get_the_category_list( __( ', ', 'storefront' ) );
+
+			if ( $categories_list ) : ?>
+				<div class="cat-links">
+					<?php
+					echo '<div class="label">' . esc_attr( __( 'Posted in', 'storefront' ) ) . '</div>';
+					echo wp_kses_post( $categories_list );
+					?>
+				</div>
+			<?php endif; // End if categories. ?>
+
+			<?php
+			/* translators: used between list items, there is a space after the comma */
+			$tags_list = get_the_tag_list( '', __( ', ', 'storefront' ) );
+
+			if ( $tags_list ) : ?>
+				<div class="tags-links">
+					<?php
+					echo '<div class="label">' . esc_attr( __( 'Tagged', 'storefront' ) ) . '</div>';
+					echo wp_kses_post( $tags_list );
+					?>
+				</div>
+			<?php endif; // End if $tags_list. ?>
+
+		<?php endif; // End if 'post' == get_post_type(). ?>
+
+			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+				<div class="comments-link">
+					<?php echo '<div class="label">' . esc_attr( __( 'Comments', 'storefront' ) ) . '</div>'; ?>
+					<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'storefront' ), __( '1 Comment', 'storefront' ), __( '% Comments', 'storefront' ) ); ?></span>
+				</div>
+			<?php endif; ?>
+		</aside>
+		<?php
+	}
 }
 
 // Adds a top bar to Storefront, before the header:
@@ -65,7 +119,21 @@ add_action('init', 'da_remove_wc_breadcrumbs');
 
 function da_remove_wc_breadcrumbs(){
     remove_action('storefront_before_content', 'woocommerce_breadcrumb', 10);
-};
+}
+
+/*
+// change cart to basket:
+// note: this does not also update any 'view cart' buttons, and if you change cart page name and slug to basket then cart cannot be found at all...
+add_filter( 'woocommerce_product_add_to_cart_text', 'da_add_to_cart_text', 10, 2 );
+
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'da_add_to_cart_text', 10, 2 ); 
+function da_add_to_cart_text( $text, $product ) {
+	$text = __( 'Add to basket', 'woocommerce' );
+	return $text;
+}
+*/
+
+
 
 // remove footer credits:
 add_action( 'init', 'custom_remove_footer_credit', 10 );
