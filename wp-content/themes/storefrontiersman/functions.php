a2b9d434dbffa2a768e05b17633c1be17a7f2301
Dan Addison <danaddisoncreative@gmail.com>
2018-08-10 16:24:11 +0100

    Enqueue font awesome brands, register social menu, add to footer

--- wp-content/themes/storefrontiersman/functions.php
+++ wp-content/themes/storefrontiersman/functions.php
@@ -29,6 +29,13 @@ add_action( 'wp_enqueue_scripts', 'da_google_fonts' );
 function my_google_fonts() {
 	wp_enqueue_style( 'da-google-fonts', 'https://fonts.googleapis.com/css?family=Montserrat:300,400" rel="stylesheet"', false ); 
 }
+
+// add font awesome brands (some fa is already in the theme eg user, home, cart icons, but not brands):
+add_action( 'wp_enqueue_scripts', 'da_social_icons_enqueue_fab' );
+
+function da_social_icons_enqueue_fab() {
+	wp_enqueue_style( 'font-awesome-5-brands', '//use.fontawesome.com/releases/v5.0.13/css/brands.css' );
+}
  
 // remove 'sort by average rating' from the dropdown on a product page (reinstate if there are eventually lots of ratings):
 add_filter ( 'woocommerce_catalog_orderby', 'da_catalog_orderby', 20);
@@ -42,36 +49,18 @@ return $orderby;
 add_action( 'init', 'da_remove_storefront_actions' );
 
 function da_remove_storefront_actions() {
+// remove secondary menu from header:
+remove_action( 'storefront_header', 'storefront_secondary_navigation', 30 );
+
 // remove search from header:
 remove_action( 'storefront_header', 'storefront_product_search', 	40 );
 
 // remove post meta (author card, category, leave a comment link) from posts:
 remove_action( 'storefront_loop_post', 'storefront_post_meta', 20 );
 remove_action( 'storefront_single_post', 'storefront_post_meta', 20 );
-}
-
-
-
-// Adds a top bar to Storefront, before the header:
-/*
-add_action( 'storefront_before_header', 'storefront_add_topbar' );
-
-function storefront_add_topbar() {
-    ?>
-    <div id="topbar">
-        <div class="col-full">
-            <p>Your text here</p>
-        </div>
-    </div>
-    <?php
-}
-*/
 
 // remove breadcrumbs:
-add_action('init', 'da_remove_wc_breadcrumbs');
-
-function da_remove_wc_breadcrumbs(){
-    remove_action('storefront_before_content', 'woocommerce_breadcrumb', 10);
+remove_action('storefront_before_content', 'woocommerce_breadcrumb', 10);
 }
 
 /*
@@ -109,7 +98,7 @@ function da_home_link() {
 // remove footer credits:
 add_action( 'init', 'da_remove_footer_credit', 10 );
 
-function custom_remove_footer_credit () {
+function da_remove_footer_credit () {
     remove_action( 'storefront_footer', 'storefront_credit', 20 );
     add_action( 'storefront_footer', 'da_storefront_credit', 20 );
 } 
@@ -118,7 +107,36 @@ function da_storefront_credit() {
 	?>
 	<div class="site-info">
         <p>&copy; <?php echo get_bloginfo( 'name' ) . ' ' . get_the_date( 'Y' ); ?></p>
-        <p>Website by <a href="https://www.danaddison.co.uk/">Dan Addison</a></p>
+        <p class="credit">Website by <a href="https://www.danaddison.co.uk/">Dan Addison</a></p>
 	</div><!-- .site-info -->
 	<?php
+}
+
+// add secondary menu to footer:
+add_action( 'storefront_footer', 'storefront_secondary_navigation', 10 );
+
+// add social menu to footer
+// first we register it by filtering the storefront_register_nav_menus and adding our new menu to that array:
+add_filter( 'storefront_register_nav_menus', 'da_storefront_register_social_menu');
+function da_storefront_register_social_menu($menus){
+	$menus['social'] =  __( 'Social Menu', 'storefront' );
+	return $menus;
+}
+// now display the social menu in the footer:
+add_action( 'storefront_footer', 'da_storefront_social_navigation', 20 );
+function da_storefront_social_navigation() {
+		if ( has_nav_menu( 'social' ) ) {
+			?>
+			<nav class="social-navigation" role="navigation" aria-label="<?php esc_html_e( 'Social Navigation', 'storefront' ); ?>">
+				<?php
+					wp_nav_menu(
+						array(
+							'theme_location'	=> 'social',
+							'fallback_cb'		=> '',
+						)
+					);
+				?>
+			</nav><!-- #site-navigation -->
+			<?php
+	}
 }
