a380a51f7bf31c9088ac9f8a5168ef06e0054b83
Dan Addison <danaddisoncreative@gmail.com>
2018-08-12 12:06:46 +0100

    Splashpage: override homepage template and set image to fill screen

---
+++ wp-content/themes/storefrontiersman/template-homepage.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Dan's template for displaying the homepage.
+ *
+ * Template name: Homepage
+ *
+ * @package storefront
+ */
+get_header(); ?>
+
+<div class="splash">
+
+  <div class="splash__image">
+		<!-- <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/testWork4.png" alt=""> -->
+	</div>
+    
+	<div class="splash__text">
+    
+    <div class="row">
+
+			<h1><?php bloginfo( 'name' ); ?></h1>
+
+			<p>Antiquarian Books, Maps & Prints</p>
+
+			<div class="splash__links">
+				
+			</div>
+
+		</div>
+
+	</div><!-- splash__text -->
+
+</div>
+
