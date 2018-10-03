ef9507df6d723d332ed8e7aa874187cc286b2f61
Dan Addison <danaddisoncreative@gmail.com>
2018-10-03 16:42:09 +0100

    Nav: move markup outside header for mobile nav + other misc.

--- wp-content/themes/storefrontiersman/functions.php
+++ wp-content/themes/storefrontiersman/functions.php
@@ -31,11 +31,23 @@ function da_google_fonts() {
 }
 
 // add font awesome brands (some fa is already in the theme eg user, home, cart icons, but not brands):
-add_action( 'wp_enqueue_scripts', 'da_social_icons_enqueue_fab' );
+add_action( 'wp_enqueue_scripts', 'da_font_awesome_enqueue' );
 
-function da_social_icons_enqueue_fab() {
-	wp_enqueue_style( 'font-awesome-5-brands', '//use.fontawesome.com/releases/v5.0.13/css/brands.css' );
+function da_font_awesome_enqueue() {
+	wp_enqueue_style( 'font-awesome-5-all', '//use.fontawesome.com/releases/v5.3.1/css/all.css' );
 }
+
+function da_move_nav_below_header_image() 
+{
+remove_action( 'storefront_header', 'storefront_primary_navigation_wrapper', 42 );
+remove_action( 'storefront_header', 'storefront_primary_navigation', 50 );
+remove_action( 'storefront_header', 'storefront_header_cart', 60 );
+add_action( 'storefront_before_content', 'storefront_primary_navigation_wrapper', 5);
+add_action( 'storefront_before_content', 'storefront_primary_navigation', 5 );
+add_action( 'storefront_before_content', 'storefront_header_cart', 5 );
+add_action( 'storefront_before_content', 'storefront_primary_navigation_wrapper_close', 5 );
+}
+add_action( 'init', 'da_move_nav_below_header_image' );
 
 // add hero image to all pages:
 /*
@@ -75,8 +87,10 @@ add_action( 'widgets_init', 'da_widgets_init' );
 	
 // create instagram feed widget:	
 function da_instagram_widget() {
+	// first, if this sidebar is not active then return (do not execute the rest of the function):
 	if( ! is_active_sidebar( 'above_footer_widget' ) )
 		return;
+	// now, display this sidebar if we're on the following pages, or on any type of events page (uses a tidy function from the Tribe Events Calendar plugin to check for all of these):
 	if ( is_page ( array( 'bio', 'contact', 'publications' ) ) || tribe_is_event_query() ) {
 		dynamic_sidebar( 'above_footer_widget' );
 	}
@@ -117,16 +131,13 @@ remove_action('storefront_before_content', 'woocommerce_breadcrumb', 10);
 // remove the sidebars on all pages and archives, except those stated:
 add_action( 'wp', 'da_remove_sidebar_shop_page' );
 function da_remove_sidebar_shop_page() {
-	
-	if ( ! is_page ( array( 'bio', 'contact' ) )) {
-		
+	if ( ! is_page ( array( 'bio', 'contact' ) )) {		
 		remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
 		add_filter( 'body_class', 'da_remove_sidebar_class_body', 10 );
 	}
 }
 
 function da_remove_sidebar_class_body( $wp_classes ) {
-	
 	$wp_classes[] = 'page-template-template-fullwidth-php';
 	return $wp_classes;
 }
@@ -137,29 +148,33 @@ function da_shop_page_sections() {
 	
 	if ( is_shop() ) {	
 		add_action( 'woocommerce_before_shop_loop', 'storefront_featured_products', 8 );
-		add_action( 'woocommerce_before_shop_loop', 'storefront_product_categories', 8 );
+		// add_action( 'woocommerce_before_shop_loop', 'storefront_product_categories', 8 );
 	}
 }
 
 // change wording of featured products section from 'we recommend':
 add_filter( 'storefront_featured_products_args', 'da_featured_products' );
 function da_featured_products( $args ){
-	$args['title'] = __( 'So Hot Right Now', 'storefront' );
-	return $args;
-}
-
-// changing number of columns for product category rows:
-add_filter( 'storefront_product_categories_args', 'da_product_categories' );
-function da_product_categories( $args ){
 	$args = array(
-		'limit' => 5,
-		'columns' => 5,
+		'limit' => 6,
+		'columns' => 3,
 		'orderby' => 'name',
-		'title'	=> __( 'Shop by Category', 'storefront' ),
+		'title'	=> __( 'Featured Products', 'storefront' ),
 	);
 	return $args;
 }
 
+// changing number of columns for product category rows, if a 'Shop by Category' section is to be included on main shop page:
+// add_filter( 'storefront_product_categories_args', 'da_product_categories' );
+// function da_product_categories( $args ){
+// 	$args = array(
+// 		'limit' => 5,
+// 		'columns' => 5,
+// 		'orderby' => 'name',
+// 		'title'	=> __( 'Shop by Category', 'storefront' ),
+// 	);
+// 	return $args;
+// }
 
 // remove phone field in checkout:
 add_filter( 'woocommerce_checkout_fields', 'da_checkout_fields', 20 );
@@ -223,15 +238,15 @@ function da_storefront_register_social_menu($menus){
 	return $menus;
 }
 
-// remove footer credits and add my own within a new footer that also includes social menu:
+// remove footer credits and add my own credits within a new footer that also includes social menu:
 add_action( 'init', 'da_custom_footer', 10 );
 
 function da_custom_footer () {
     remove_action( 'storefront_footer', 'storefront_credit', 20 );
-		add_action( 'storefront_footer', 'da_storefront_footer', 20 );
+		add_action( 'storefront_footer', 'da_sub_footer', 20 );
 } 
 
-function da_storefront_footer() {
+function da_sub_footer() {
 	?>
 	<div class="sub-footer">
 
