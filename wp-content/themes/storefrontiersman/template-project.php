f3a8b0248897c29c9f14c0b8ab8c4bb32c55ea76
Dan Addison <danaddisoncreative@gmail.com>
2018-10-05 12:21:10 +0100

    Reorganise sass directory

--- wp-content/themes/storefrontiersman/template-project.php
+++ wp-content/themes/storefrontiersman/template-project.php
@@ -20,24 +20,73 @@ get_header(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+				<a href="<?php $parentLink = get_permalink($post->post_parent); echo $parentLink; ?>" title="Go back to the Portfolio page" class="back">&#8592; Back to Work</a>
+
+				<header>
+					<?php the_title( '<h1 class="artwork-title">', '</h1>' ); ?>
+				</header>
+
+
 					<?php
-					/**
-					 * Functions hooked in to storefront_page add_action
-					 *
-					 * @hooked storefront_page_header          - 10
-					 * @hooked storefront_page_content         - 20
-					 */
-					do_action( 'storefront_page' ); ?>
+						$fields = get_field_objects();
+						
+						if( $fields ) : ?>
+
+						<div class="artwork-meta">
+
+							<p class="artwork-date"><?php the_field('artwork_date'); ?></p>
+							<p class="artwork-materials"><?php the_field('artwork_materials'); ?></p>
+							
+							<?php if(get_field('artwork_collaborators')) : ?>
+								<p><?php the_field('artwork_collaborators'); ?></p>
+							<?php endif; ?>
+
+						</div><!-- .artwork-meta info -->
+
+						<div class="artwork-images">
+
+							<?php			
+							$imageArray = get_field('artwork_featured_image'); // Array returned by Advanced Custom Fields
+							$imageAlt = esc_attr($imageArray['alt']); // Grab, from the array, the 'alt'
+							$imageMediumLargeURL = esc_url($imageArray['sizes']['medium_large']); //grab from the array, the 'sizes', and from it, the 'medium_large'
+							?>
+							
+							<div class="artwork-featured-image">
+								<img src="<?php echo $imageMediumLargeURL;?>" alt="<?php echo $imageAlt; ?>">
+							</div><!-- .artwork-image - main feature image -->
+
+						</div><!-- .artwork-images container -->
+
+						<div class="artwork-description">
+							<p><?php the_field('artwork_description'); ?></p>
+						</div>
+
+						<div class="artwork-thumbnails-gallery">
+
+							<?php	while ( have_rows('artwork_thumbnail_gallery') ) : the_row(); 
+								$image = get_sub_field('thumbnail_images');
+								$thumbnail = 'thumbnail';
+								$large = 'large';
+								$video = get_sub_field('video_links');
+							?>
+
+							<?php if( $video ) : ?>
+							<a href="<?php echo ( $video )?>" data-fancybox="group">
+								<img src="<?php echo wp_get_attachment_image_url( $image, $thumbnail )?>" alt="<?php echo $image['alt']; ?>" class="thumbnail-image">
+							</a>
+							<?php else : ?>
+							<a href="<?php echo wp_get_attachment_image_url( $image, $large )?>" alt="<?php echo $image['alt']; ?>" data-fancybox="group">
+								<img src="<?php echo wp_get_attachment_image_url( $image, $thumbnail )?>" alt="<?php echo $image['alt']; ?>" class="thumbnail-image">
+							</a>
+							<?php endif; ?>
+
+							<?php endwhile; ?>
+
+						</div><!-- .artwork-thumbnails-gallery -->
+
+					<?php endif; ?>
 
 				</article><!-- #post-## -->
-
-				<?php
-				/**
-				 * Functions hooked in to storefront_page_after action
-				 *
-				 * @hooked storefront_display_comments - 10
-				 */
-				do_action( 'storefront_page_after' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
